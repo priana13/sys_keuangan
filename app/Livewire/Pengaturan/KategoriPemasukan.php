c98c177ec856d2ca34b2d09391b13e96405d827d
priana13 <herian.sap@gmail.com>
2023-10-07 06:37:42 +0700

    update

--- app/Livewire/Pengaturan/KategoriPemasukan.php
+++ app/Livewire/Pengaturan/KategoriPemasukan.php
@@ -2,15 +2,92 @@
 
 namespace App\Livewire\Pengaturan;
 
-use App\Models\Kategori;
 use Livewire\Component;
+use App\Models\Kategori;
+use Livewire\WithPagination;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 
 class KategoriPemasukan extends Component
 {
+    use WithPagination;
+    use LivewireAlert;
+
+    public $nama;
+    public $selected_id;
+    public $action_type = "create";
+
+    protected $listeners = [
+        'confirmed'
+    ];
+
     public function render()
     {
         $data['kategori_pemasukan'] = Kategori::pemasukan()->get();
 
         return view('livewire.pengaturan.kategori-pemasukan', $data);
     }
+
+    public function create(){
+
+        $this->validate([
+            'nama' => "required|unique:kategori"
+        ]);
+
+        Kategori::create([
+            "nama" => $this->nama,
+            "type" => "Pemasukan"
+        ]);
+
+        $this->reset();
+    }
+
+    public function hapus($id){
+
+        $this->selected_id = $id;  
+
+        $this->alert('warning', 'Yakin ingin dihapus?', [
+            'showConfirmButton' => true,
+            'confirmButtonText' => 'Hapus',
+            'showCancelButton' => true,
+            'confirmButtonColor' => '#ed4e73',
+            'cancelButtonText' => 'Cancel',
+            'toast' => false,
+            'position' => 'center',
+            'onConfirmed' => 'confirmed',
+            'timer' => null
+        ]); 
+
+    }
+
+
+    public function confirmed()
+    {
+        $record = Kategori::findOrFail($this->selected_id);
+
+        $record->delete();
+
+        $this->alert('success', "Data Berhasil Dihapus");
+    }
+
+    public function edit($id){
+
+        $this->selected_id = $id;
+
+        $kategory = Kategori::find($id);
+
+        $this->nama = $kategory->nama;
+
+        $this->action_type = "update";
+    }
+
+    public function update()
+    {
+        $record = Kategori::findOrFail($this->selected_id);
+
+        $record->nama = $this->nama;
+        $record->save();
+
+        $this->alert('success', "Data Berhasil Diupdate");
+    }
+ 
 }
