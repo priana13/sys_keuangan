59e7dd91446c766ab35cead5a703cc28c677c3b0
priana13 <herian.sap@gmail.com>
2024-06-02 17:46:30 +0700

    install font awosome

--- resources/views/livewire/transaksi/over-view-transaksi.blade.php
+++ resources/views/livewire/transaksi/over-view-transaksi.blade.php
@@ -1,9 +1,13 @@
-<div class="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 my-4">
+<div class="grid grid-cols-1 gap-4 md:grid-cols-3 md:gap-6 xl:grid-cols-4 2xl:gap-7.5 my-4">
    
    
-    <div class="rounded-lg border border-stroke bg-green-800 text-white py-1 px-3 shadow-sm dark:border-strokedark dark:bg-boxdark">
-              
-        <div class="text-center">
+    <div class="rounded-lg border border-stroke bg-sky-300 py-2 px-12 shadow-sm dark:border-strokedark dark:bg-boxdark flex justify-items-center">
+        <div class="flex justify-items-center text-gray-500">
+           <i class="text-3xl fa-solid fa-arrow-down-wide-short my-auto"></i>
+        </div>
+        
+        <div class="text-center mx-auto">
+           
             <span class="text-sm font-medium mb-2">Pemasukan</span>
             <h4 class="text-title-md font-bold dark:text-white">
             Rp. {{ number_format($total_pemasukan ,0,',', '.')  }}
@@ -14,9 +18,13 @@
     </div>
 
 
-    <div class="rounded-lg border border-stroke bg-orange-800 text-white py-1 px-3 shadow-sm dark:border-strokedark dark:bg-boxdark">
+    <div class="rounded-lg border border-stroke bg-orange-300 py-1 px-3 shadow-sm dark:border-strokedark dark:bg-boxdark flex justify-items-center">
+
+        <div class="flex justify-items-center text-gray-500">
+           <i class="text-3xl fa-solid fa-arrow-up-wide-short my-auto"></i>
+        </div>
               
-        <div class="text-center">
+        <div class="text-center mx-auto">
             <span class="text-sm font-medium mb-2">Pengeluaran</span>
             <h4 class="text-title-md font-bold dark:text-white">
             Rp. {{ number_format($total_pengeluaran ,0,',', '.')  }}
